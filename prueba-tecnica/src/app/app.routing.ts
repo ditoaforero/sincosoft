f4e5d5bf76f7e99b850a69080af4a76130e205f6
ditoaforero <ditoaforero@hotmail.com>
2018-09-01 18:34:46 -0500

    Se carga listar alumno

--- prueba-tecnica/src/app/app.routing.ts
+++ prueba-tecnica/src/app/app.routing.ts
@@ -8,12 +8,15 @@ import { ListarAlumnosComponent } from '../app/components/alumno/listar/listar.c
 import { ListarMateriasComponent } from '../app/components/materia/listar/listar.component';
 import { ListarProfesoresComponent } from '../app/components/profesor/listar/listar.component';
 
+import { AgregarAlumnoComponent } from '../app/components/alumno/agregar/agregar.component';
+
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch:'full' },
   { path: 'home', component: HomeComponent },
   { path: 'alumnos', component: ListarAlumnosComponent },
   { path: 'materias', component: ListarMateriasComponent },
   { path: 'profesores', component: ListarProfesoresComponent },
+  { path: 'agregarAlumno', component: AgregarAlumnoComponent },
   { path: '**', component: HomeComponent }
 
 ];
--- prueba-tecnica/src/app/components/alumno/listar/listar.component.ts
+++ prueba-tecnica/src/app/components/alumno/listar/listar.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarAlumnosComponent implements OnInit {
 
+  private alumnos = [
+    {
+      nombre:'Eduardo',
+      primerApellido:'Arevalo',
+      segundoApellido:'Forero',
+      curso: 10
+    },
+    {
+      nombre:'Lilina',
+      primerApellido:'Manjarres',
+      segundoApellido:'Villanueva',
+      curso: 11
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
