dad7568093bcd0f6d5cdb7ac9f0aa41796fbd830
Pandita <lmanjarres@stratio.com>
2018-09-02 14:37:37 -0500

    se agrega codigo para editar materias

--- prueba-tecnica/src/app/components/materia/editar/editar.component.ts
+++ prueba-tecnica/src/app/components/materia/editar/editar.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Materia } from '../../../models/materia';
+import { MateriaService } from '../../../services/materia.service';
 
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html'
 })
 export class EditarMateriaComponent implements OnInit {
+  idMateria: string;
+  materias: Array<Materia>;
+  materia: Materia;
+  constructor(private route: ActivatedRoute, private _materiaService: MateriaService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.idMateria = params['id'];
+    });
+    this.materias= this._materiaService.obtenerListado().filter(m=> m.id == this.idMateria);
+
+    if(this.materias.length>0){
+      this.materia= this.materias[0]
+    }else{
+      console.log("no encontro materia para editar!!")
+    }
+  }
+
+  editar() {
+    this._materiaService.actualizarMateria(this.materia);
+    this.materia = new Materia();
+    document.querySelector('form').reset();
+    alert("Materia actualizada!")
+  }
 }
